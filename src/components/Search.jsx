f0083d4360d9a5c6941fd7208ab047fee0121046
Nasrul Fuad <nasrulfuad08@gmail.com>
2020-08-23 22:35:11 +0700

    hiddenable button search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,30 +1,44 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core";
 import "../styles/Search.css";
 
-function Search() {
+function Search({ hideButtons = false }) {
     const [input, setInput] = useState("");
 
+    const history = useHistory();
+
     const search = e => {
-        e.prefentDefault();
+        e.preventDefault();
+        history.push("/search");
     };
 
     return (
-        <div className="search">
+        <form className="search">
             <div className="search__input">
                 <SearchIcon className="search__inputIcon" />
                 <input value={input} onChange={e => setInput(e.target.value)} />
                 <MicIcon />
             </div>
             <div className="search__buttons">
-                <Button variant="outlined" onClic={search}>
+                <Button
+                    className={hideButtons ? "search__buttonsHidden" : ""}
+                    type="submit"
+                    variant="outlined"
+                    onClick={search}
+                >
                     Google Search
                 </Button>
-                <Button variant="outlined">I'm feeling Lucky</Button>
+                <Button
+                    className={hideButtons ? "search__buttonsHidden" : ""}
+                    variant="outlined"
+                >
+                    I'm feeling Lucky
+                </Button>
             </div>
-        </div>
+        </form>
     );
 }
 
