84143cb636fcc07fc80407e700995212dfcd1a99
Nasrul Fuad <nasrulfuad08@gmail.com>
2020-08-24 10:34:15 +0700

    Styling result page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import RoomIcon from "@material-ui/icons/Room";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 import { Link } from "react-router-dom";
 import { useStateValue } from "../react_context/StateProvider";
 import GoogleSearch from "../components/GoogleSearch";
 import "../styles/SearchPage.css";
 
 import data from "../response.json";
+import Search from "../components/Search";
 
 function SearchPage() {
     const [{ term }, dispatch] = useStateValue();
@@ -21,8 +29,72 @@ function SearchPage() {
                         alt="google-logo"
                     />
                 </Link>
+                <div className="searchPage__headerBody">
+                    <Search hideButtons />
+                    <div className="searchPage__options">
+                        <div className="searchPage__optionsLeft">
+                            <div className="searchPage__option">
+                                <SearchIcon />
+                                <Link to="/all">All</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <DescriptionIcon />
+                                <Link to="/new">News</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <ImageIcon />
+                                <Link to="/images">Images</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <LocalOfferIcon />
+                                <Link to="/shopping">Shopping</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <RoomIcon />
+                                <Link to="/maps">Maps</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <MoreVertIcon />
+                                <Link to="/more">More</Link>
+                            </div>
+                        </div>
+                        <div className="searchPage__optionsRight">
+                            <div className="searchPage__option">
+                                <Link to="/settings">Settings</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <Link to="/tools">Tools</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="searchPage__results"></div>
+            {true && (
+                <div className="searchPage__results">
+                    <p className="searchPage__resultCount">
+                        About {data?.searchInformation.formattedTotalResults}
+                        results ({data?.searchInformation.formattedSearchTime}
+                        seconds) for {term}
+                    </p>
+                    {data?.items.map(item => (
+                        <div className="searchPage__result">
+                            <a href={item.link}>
+                                {item.displayLink}
+                                <ArrowDropDownIcon fontSize="small" />
+                            </a>
+                            <a
+                                className="searchPage__resultTitle"
+                                href={item.link}
+                            >
+                                <h2>{item.title}</h2>
+                            </a>
+                            <p className="searchPage__resultSnippet">
+                                {item.snippet}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
